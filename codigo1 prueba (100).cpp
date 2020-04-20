135678b7fded1bfec034df3cc0662e10fd05efa8
MiguelT <miguel.ats26@gmail.com>
2020-04-20 18:36:18 -0500

    segunda version.

--- codigo1 prueba (100).cpp
+++ codigo1 prueba (100).cpp
@@ -3,15 +3,15 @@
 using namespace std;
 
 int main(){
-	float N1,n2;
+	float N1,n2,N3;
 	
 	cout<<"digite un numero para dividirlo entre 100"<<endl;
 	cin>>N1;
 	
 	n2=(N1/100);
-	
-	cout<<"\nel resultado de la division es: "<<n2;
-	
+	N3=(n2+5);
+	cout<<"\nel resultado de la division es: "<<n2<<endl;
+	cout<<"le sume 5 al resultado, ahora es: "<<N3<<endl;
 	
 	
 	return 0;
