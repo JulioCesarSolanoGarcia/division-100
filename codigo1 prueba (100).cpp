5a391989de201a5933f0a04e7b0773d1e9c5b0b9
MiguelT <miguel.ats26@gmail.com>
2020-04-20 18:27:02 -0500

    primera version.

---
+++ codigo1 prueba (100).cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	float N1,n2;
+	
+	cout<<"digite un numero para dividirlo entre 100"<<endl;
+	cin>>N1;
+	
+	n2=(N1/100);
+	
+	cout<<"\nel resultado de la division es: "<<n2;
+	
+	
+	
+	return 0;
+}
